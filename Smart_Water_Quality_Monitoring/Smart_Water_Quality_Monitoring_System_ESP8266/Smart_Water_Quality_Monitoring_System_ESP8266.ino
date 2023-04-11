f9a601c921e8fac293e69414e8c8c2938e7776d8
Onkar Choudhari <64524540+OnkarSC@users.noreply.github.com>
2023-04-11 22:04:01 +0530

    Add files via upload

---
+++ Smart_Water_Quality_Monitoring/Smart_Water_Quality_Monitoring_System_ESP8266/Smart_Water_Quality_Monitoring_System_ESP8266.ino
@@ -0,0 +1,63 @@
+ //NodeMCU Code:
+ #include <ESP8266HTTPClient.h>
+ #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#include<ESP8266WiFi.h>
+#include<WiFiClient.h>
+#include<ESP8266WebServer.h>
+#include <ArduinoJson.h>
+const char* ssid = "project";//Replace with your network SSID
+const char* password = "1234567890";//Replace with your network password
+ESP8266WebServer server(80);
+String page = "";
+int data1, data2, data3;
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin();
+  lcd.print("IoT Base Hydro-");
+  lcd.setCursor(0,1);
+  lcd.print("ponics System");
+  WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED)
+  {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+  lcd.clear();
+  lcd.print("IP address");
+  lcd.setCursor(0,1);
+  lcd.print(WiFi.localIP());
+  server.on("/", []()
+  {
+    page = "<html><head><title>tech vision</title></head><style type=\"text/css\">";
+    page += "table{border-collapse: collapse;}th {background-color:  skyblue ;color: red;}table,td {border: 4px solid black;font-size: x-large;";
+    page += "text-align:center;border-style: groove;border-color: rgb(255,0,0);}</style><body><center>";
+    page += "<h1>IoT Base Hydroponics System</h1><br><br><table style=\"width: 1200px;height: 450px;\"><tr>";
+    page += "<th>Parameters</th><th>Value</th><th>Units</th></tr><tr><td>PH Value</td><td>"+String(data1)+"</td><td>N/A</td></tr>";
+    page += "<tr><td>Temperature</td><td>"+String(data2)+"</td><td>Centigrade</td></tr><tr><td>Moisture</td><td>"+String(data3)+"</td><td>%</td>";
+    page += "<meta http-equiv=\"refresh\" content=\"3\">";
+    server.send(200, "text/html", page);
+  });
+  server.begin();
+}
+void loop()
+{
+  StaticJsonBuffer <1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(Serial);
+  if (root == JsonObject::invalid())
+  {
+    return;
+    Serial.println("invalid");
+  }
+  data1 = root["a1"];
+  data2 = root["a2"];
+  data3 = root["a3"];
+  Serial.println(data1);
+  Serial.println(data2);
+  Serial.println(data3);
+  server.handleClient();
+}
